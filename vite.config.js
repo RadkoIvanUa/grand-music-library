f1fd6d7bae0a403f192d9a14e363d06dded71c12
Ivan Radko <mr.radkoivan@gmail.com>
2023-07-31 10:47:22 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/grand-music-library/",
+  base: "/grand-music-library",
 });
