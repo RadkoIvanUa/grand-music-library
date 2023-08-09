3f9ba1989f0c6b7d10c746ae9a2c991ca1f4c280
Ivan Radko <mr.radkoivan@gmail.com>
2023-08-09 20:22:12 +0300

    commit

--- src/pages/Library/Library.jsx
+++ src/pages/Library/Library.jsx
@@ -1,22 +1,23 @@
 import AudioPlayer from "../../components/AudioPlayer/AudioPlayerItem";
 
+import music from "../../music/Adventure.mp3";
+
+import { StyledContainer } from "../../components/Container/StyledContainer";
 export default function Library() {
   return (
-    <ul>
-      <li>
-        <AudioPlayer
-          audioSrc={
-            "https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3"
-          }
-        />
-      </li>
-      <li>
-        <AudioPlayer
-          audioSrc={
-            "https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3"
-          }
-        />
-      </li>
-    </ul>
+    <StyledContainer>
+      <ul>
+        <li>
+          <AudioPlayer
+            url={
+              "https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3"
+            }
+          />
+        </li>
+        <li>
+          <AudioPlayer url={music} />
+        </li>
+      </ul>
+    </StyledContainer>
   );
 }
