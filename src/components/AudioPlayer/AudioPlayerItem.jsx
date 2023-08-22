2a55fcacedac418ff8a6798200c04998f6933c9c
Ivan Radko <mr.radkoivan@gmail.com>
2023-08-22 11:58:21 +0300

    add player

--- src/components/AudioPlayer/AudioPlayerItem.jsx
+++ src/components/AudioPlayer/AudioPlayerItem.jsx
@@ -1,45 +1,131 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react-hooks/exhaustive-deps */
-import {
-  Box,
-  Card,
-  CardContent,
-  IconButton,
-  Slider,
-  Typography,
-} from "@mui/material";
+import { Box, Card, IconButton, Slider } from "@mui/material";
 import { useRef, useState } from "react";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import PauseIcon from "@mui/icons-material/Pause";
 import { useTheme } from "@mui/material/styles";
 import ReactPlayer from "react-player";
+import { BsCloudDownload } from "react-icons/bs";
 
 // eslint-disable-next-line react/prop-types
-export default function AudioPlayer({ url }) {
-  const [currentUrl, setCurrentUrl] = useState(null);
-  const playerRef = useRef(null);
 
-  const playAudio = (newUrl) => {
-    if (currentUrl === newUrl) {
-      return; // Не змінювати, якщо вже грає ця аудіозапис
-    }
+export default function AudioPlayer({ data, onTogglePlay }) {
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const audioRef = useRef(null);
 
-    if (playerRef.current) {
-      playerRef.current.seekTo(0); // Перемотувати поточний плеєр на початок
-      playerRef.current.pause(); // Призупинити відтворення поточного плеєра
-    }
+  const theme = useTheme();
+  theme.palette.mode === "dark" ? "rgba(255,255,255,0.4)" : "rgba(0,0,0,0.4)";
 
-    setCurrentUrl(newUrl); // Оновити URL активного плеєра
+  const handleSeek = (e) => {
+    setCurrentTime(e.target.value);
+    audioRef.current.seekTo(e.target.value);
   };
 
+  function formatDuration(durationSeconds) {
+    const minutes = Math.floor(durationSeconds / 60);
+    const seconds = Math.floor(durationSeconds % 60);
+    const formattedSeconds = seconds.toString().padStart(2, "0");
+    return `${minutes}:${formattedSeconds}`;
+  }
+
   return (
-    <div>
-      <ReactPlayer
-        ref={playerRef}
-        url={url}
-        playing={url === currentUrl}
-        controls
-      />
-      <button onClick={() => playAudio(url)}>Відтворити</button>
-    </div>
+    <>
+      <p style={{ color: "#fff" }}>{data.title}</p>
+      <Card
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: "10px",
+          marginBottom: "10px",
+          padding: "10px",
+        }}
+      >
+        <ReactPlayer
+          url={data.src}
+          playing={!data.isPaused}
+          ref={audioRef}
+          height={0}
+          width={0}
+          onDuration={(duration) => {
+            setDuration(duration);
+          }}
+          onProgress={(process) => {
+            setCurrentTime(process.playedSeconds);
+          }}
+        />
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <IconButton onClick={() => onTogglePlay(data)}>
+            {data.isPaused ? (
+              <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+            ) : (
+              <PauseIcon sx={{ height: 38, width: 38 }} />
+            )}
+          </IconButton>
+        </Box>
+        <p style={{ fontSize: 20, paddingTop: 8 }}>
+          {formatDuration(currentTime)}
+        </p>
+        <Box sx={{ width: "100%" }}>
+          <Slider
+            aria-label="time-indicator"
+            size="small"
+            type="range"
+            min={0}
+            step={1}
+            max={duration}
+            onChange={handleSeek}
+            value={currentTime}
+            sx={{
+              color:
+                theme.palette.mode === "dark" ? "#fff" : "rgba(0,0,0,0.87)",
+              height: 4,
+              padding: 0,
+              "& .MuiSlider-thumb": {
+                width: 8,
+                height: 8,
+                transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
+                "&:before": {
+                  boxShadow: "0 2px 12px 0 rgba(0,0,0,0.4)",
+                },
+                "&:hover, &.Mui-focusVisible": {
+                  boxShadow: `0px 0px 0px 8px ${
+                    theme.palette.mode === "dark"
+                      ? "rgb(255 255 255 / 16%)"
+                      : "rgb(0 0 0 / 16%)"
+                  }`,
+                },
+                "&.Mui-active": {
+                  width: 20,
+                  height: 20,
+                },
+              },
+              "& .MuiSlider-rail": {
+                opacity: 0.28,
+              },
+            }}
+          />
+        </Box>
+        <p style={{ fontSize: 20, paddingTop: 8 }}>
+          {formatDuration(duration)}
+        </p>
+        <IconButton>
+          <a
+            href={data.src}
+            style={{
+              width: 30,
+              height: 30,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            download
+          >
+            <BsCloudDownload color="red" />
+          </a>
+        </IconButton>
+      </Card>
+    </>
   );
 }
--- src/pages/Library/Library.jsx
+++ src/pages/Library/Library.jsx
@@ -3,21 +3,39 @@ import AudioPlayer from "../../components/AudioPlayer/AudioPlayerItem";
 import music from "../../music/Adventure.mp3";
 
 import { StyledContainer } from "../../components/Container/StyledContainer";
+import { useState } from "react";
 export default function Library() {
+  const initialAudioData = [
+    {
+      title: "Elephant",
+      src: "https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3",
+      isPaused: true,
+    },
+    { title: "Epic Adventure", src: music, isPaused: true },
+    {
+      title: "Elephant",
+      src: "https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3",
+      isPaused: true,
+    },
+  ];
+
+  const [audioData, setAudioData] = useState(initialAudioData);
+
+  const handleTogglePlay = (clickedData) => {
+    const updatedAudioData = audioData.map((data) => {
+      if (data === clickedData) {
+        return { ...data, isPaused: !data.isPaused };
+      }
+      return { ...data, isPaused: true };
+    });
+    setAudioData(updatedAudioData);
+  };
+
   return (
     <StyledContainer>
-      <ul>
-        <li>
-          <AudioPlayer
-            url={
-              "https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3"
-            }
-          />
-        </li>
-        <li>
-          <AudioPlayer url={music} />
-        </li>
-      </ul>
+      {audioData.map((data, index) => (
+        <AudioPlayer key={index} data={data} onTogglePlay={handleTogglePlay} />
+      ))}
     </StyledContainer>
   );
 }
